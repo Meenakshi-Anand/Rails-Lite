8fc4fd67a6d18a0ea33e62352babe23af10fba6d
Meenakshi-Anand <abhi@codeanand.com>
2018-07-18 23:13:33 -0700

    rails lite project

---
+++ lib/controller_base.rb
@@ -0,0 +1,57 @@
+require 'active_support'
+require 'active_support/core_ext'
+require 'erb'
+require_relative './session'
+require 'byebug'
+
+class ControllerBase
+  attr_reader :req, :res, :params
+
+  # Setup the controller
+  def initialize(req, res)
+    @req = req
+    @res = res
+    @already_built_response = false
+  end
+
+  # Helper method to alias @already_built_response
+  def already_built_response?
+     @already_built_response
+  end
+
+  # Set the response status code and header
+  def redirect_to(url)
+    raise "double render error" if self.already_built_response?
+    @res['Location'] = url
+    @res.status = 302
+    @already_built_response = true
+  end
+
+  # Populate the response with content.
+  # Set the response's content type to the given type.
+  # Raise an error if the developer tries to double render.
+  def render_content(content, content_type)
+    raise "double render error" if self.already_built_response?
+    @res.write(content)
+    @res['Content-Type']= content_type
+    @res.finish
+    @already_built_response = true
+  end
+
+  # use ERB and binding to evaluate templates
+  # pass the rendered html to render_content
+  def render(template_name)
+    controller_name = self.class.to_s.underscore
+    file_content = File.read("views/#{controller_name}/#{template_name}.html.erb")
+    template = ERB.new(file_content).result(binding)
+    render_content(template,'text/html')
+  end
+
+  # method exposing a `Session` object
+  def session
+  end
+
+  # use this with the router to call action_name (:index, :show, :create...)
+  def invoke_action(name)
+  end
+end
